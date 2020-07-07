d94f88314b6eab09877946949bf1aebfa11e6fbd
Enron Hubbard <tygerhardt@gmail.com>
2020-07-06 18:33:17 -0700

    martsyalis'Added catch opject to catch any errors.

--- index.js
+++ index.js
@@ -2,62 +2,65 @@ const inquirer = require("inquirer");
 const axios = require("axios");
 const fs = require('fs');
 const path = require('path');
-async function main(){
+
+async function main() {
+    try{
     console.log(`starting`);
-    const userResponse = await inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "What is your GitHub user name?",
-            name: "username"
-        },
-        {
-            type: "input",
-            message: "What is your Project Tittle?",
-            name: "projectTittle"
-        },
-        {
-            type: "input",
-            message: "Provide detail description",
-            name: "projectDescription"
-        },
-        {
-            type: "input",
-            message: "Enter table of contents",
-            name: "tableOfContents"
-        },
-        {
-            type: "input",
-            message: "Provide installation steps",
-            name: "installation"
-        },
-        {
-            type: "input",
-            message: "Provide usage details",
-            name: "usage"
-        },
-        {
-            type: "input",
-            message: "Provide license name",
-            name: "licenseName"
-        },
-       
-        {
-            type: "input",
-            message: "Please enter git hub user names of the contributor if any (If there are mulitple contributor, seperate names with comma and no space! )",
-            name: "contributorsGitUserName"
-        },
-        {
-            type: "input",
-            message: "Provide examples on how to run tests.",
-            name: "tests"
-        },
-		 {
-            type: "input",
-            message: "Any questions?",
-            name: "questions"
-        }
-        ]);
+
+        const userResponse = await inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is your GitHub user name?",
+                    name: "username"
+                },
+                {
+                    type: "input",
+                    message: "What is your Project Tittle?",
+                    name: "projectTittle"
+                },
+                {
+                    type: "input",
+                    message: "Provide detail description",
+                    name: "projectDescription"
+                },
+                {
+                    type: "input",
+                    message: "Enter table of contents",
+                    name: "tableOfContents"
+                },
+                {
+                    type: "input",
+                    message: "Provide installation steps",
+                    name: "installation"
+                },
+                {
+                    type: "input",
+                    message: "Provide usage details",
+                    name: "usage"
+                },
+                {
+                    type: "input",
+                    message: "Provide license name",
+                    name: "licenseName"
+                },
+
+                {
+                    type: "input",
+                    message: "Please enter git hub user names of the contributor if any (If there are mulitple contributor, seperate names with comma and no space! )",
+                    name: "contributorsGitUserName"
+                },
+                {
+                    type: "input",
+                    message: "Provide examples on how to run tests.",
+                    name: "tests"
+                },
+                {
+                    type: "input",
+                    message: "Any questions?",
+                    name: "questions"
+                }
+            ])
         console.log(`starting`);
         console.log(userResponse);
         const gitUsername = userResponse.username;
@@ -69,25 +72,28 @@ async function main(){
         const licenseName = userResponse.licenseName;
         const contributorUserNames = userResponse.contributorsGitUserName;
         const tests = userResponse.tests;
-		const questions = userResponse.questions;
-            // fetching data from git
-            // user
-        const gitResponse = await axios.get(`https://api.github.com/users/${gitUsername}`);
+        const questions = userResponse.questions;
+        // fetching data from git
+        // user
+        const gitResponse = await axios.get(`https://api.github.com/users/${gitUsername}`)
         const gitData = gitResponse.data;
         const gitName = gitData.login;
         const gitEmail = gitData.email;
         const gitlocation = gitData.location;
         const gitUrl = gitData.html_url;
         const gitProfileImage = gitData.avatar_url;
-            // contributor
+        // contributor
         const contributorUserNamesArray = contributorUserNames.split(",");
         console.log(contributorUserNamesArray);
         // const  = listOfContributorsUserNames.
         // contributorsGitUserName
         var resultContributor = '';
-        for (i=0; i<contributorUserNamesArray.length; i++){
+
+        // martsyalis
+        for (i = 0; i < contributorUserNamesArray.length; i++) {
             var contributorsGitUserName = contributorUserNamesArray[i]
-            const gitResponse2 = await axios.get(`https://api.github.com/users/${contributorsGitUserName}`);
+            console.log('contributorsGitUserName is: ', contributorsGitUserName);
+            const gitResponse2 = await axios.get(`https://api.github.com/users/${contributorsGitUserName}`)
             var gitContribuProfileImage = gitResponse2.data.avatar_url;
             var gitContribuUrl = gitResponse2.data.html_url;
             var gitContribuEmail = gitResponse2.data.email;
@@ -108,7 +114,12 @@ async function main(){
 \n![ProfileImage](${gitProfileImage})
 \nEmail: ${gitEmail}
 `)
-var writeResult = fs.writeFileSync(path.join(__dirname, '../out', 'readMe.md'), result )
-console.log("file generated....")
-    }
+
+        var writeResult = await fs.writeFileSync(path.join(__dirname, '/output', '/readMe.md'), result)
+        console.log("file generated....")
+
+} catch(err){
+    console.error('error in catch block is: ', err);
+}
+}
 main();
---
+++ output/readMe.md
@@ -0,0 +1,17 @@
+
+# homework_week_9_readme_generator 
+# hey there
+# what;s up
+# step 1
+# sdflfsjdflsj
+# MIT license
+# 
+            
+ <img src="https://avatars3.githubusercontent.com/u/27053761?v=4" alt="drawing" width="150" display="inline"/> martsyalis  GitHubLink: https://github.com/Martsyalis
+# run it fast
+# no
+# Author Details 
+
+![ProfileImage](https://avatars3.githubusercontent.com/u/63885719?v=4)
+
+Email: null
